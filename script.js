4eadd6f194b3c32f7e459beeb6d5b210ba57cc7f
Dionte Driskell <driskell06@gmail.com>
2024-05-04 14:27:00 -0400

    changed click to submit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const registrationForm = document.getElementById|("registration");
+const registration = document.getElementById("registration");
 
-registrationForm.addEventListener("click", (e) => {
-    e.preventDefault();
-alert (`you submitted this: ${registrationForm.querySelector (`username`).value}`)
-})
+registration.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  alert(`you submitted this: ${registration.querySelector("input").value}`);
+});
+// Example above
+
+// const errorDisplay = document.getElementById("errorDisplay");
