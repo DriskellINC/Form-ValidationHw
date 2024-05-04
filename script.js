444b42ec17a9c182bbb681da31e20d6030c228cb
Dionte Driskell <driskell06@gmail.com>
2024-05-04 18:20:16 -0400

    finally touches

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const registration = document.getElementById("registration");
 
-registration.addEventListener("click", (e) => {
+registration.addEventListener("submit", (e) => {
   e.preventDefault();
 
   alert(`you submitted this: ${registration.querySelector("input").value}`);
 });
-// Example above
+// Example above part 1-3/1- 5
+//*validating the username, email and password using localStorage
+function validUsername(e) {
+  const username = document.getElementsByName("name");
+  const email = document.getElementsByName("email");
+  const password = document.getElementsByName("password");
+}
+localStorage.setItem("validUsername", ",");
+
+// Validate lowercase
+validUsername = function () {
+  // Validate lowercase letters
+  let lowerCaseLetters = /[a-z]/g;
+  if (myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+};
+//////part 4
 
 // const errorDisplay = document.getElementById("errorDisplay");
